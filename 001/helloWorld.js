f5755aaebe13621fb418d6240786a1c50ca0d28d
Ibrahim Cesar <email@ibrahimcesar.com>
2021-07-09 18:47:08 -0300

    ✨ Olá Mundo!
    Nosso primeiro arquivo

---
+++ 001/helloWorld.js
@@ -0,0 +1,10 @@
+/*
+ 💡 Olá! Este é nosso primeiro arquivo.
+  Pode ver que este é um comentário de múlttiplas linhas em contrapartida com um comentário de linha única que utiliza barras duplas "//"
+  Os comentários existirão para contextualizar melhor. Este é nosso arquivo de "Olá Mundo!" ou "Hello World!" em que testamos dois ambientes
+  ou runtimes em que o JavaScript é utilizado: dentro do navegador ou browser e em uma máquina ou servidor, através do NodeJS ou apenas Node.
+*/
+
+
+// alert("Olá Mundo!")
+console.log("Olá Mundo!");
